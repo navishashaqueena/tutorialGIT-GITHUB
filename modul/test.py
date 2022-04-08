baf601aabd470efbc410ab484e487e6ed3d942c6
navishashaqueena <rifki.kide@gmail.com>
2022-04-08 16:38:59 +0700

    modifkasi code

--- modul/test.py
+++ modul/test.py
@@ -1,14 +1,15 @@
 class ObjekData:
 
-    def __init__(self, nama, phone, email) -> None:
+    def __init__(self, nama, phone, email, nik) -> None:
         self.nama = nama
         self.phone = phone
         self.email = email
+        self.nik = nik
 
     def talk(self):
-        return f'nama : {self.nama}, no. tlp : {self.phone}, email : {self.email}'
+        return f'nama : {self.nama}, no. tlp : {self.phone}, email : {self.email}, nik : {self.nik}'
 
 
-data = ObjekData('rifki', 4356915, 'dwhiny@yahoo.com')
+data = ObjekData('rifki', 4356915, 'dwhiny@yahoo.com', 3175071510770001)
 
 print(data.talk())
